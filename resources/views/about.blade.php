73566eece22cf2be0989c72c41cf5307eaed34fd
piioni <rangelc.juane@gmail.com>
2025-10-23 03:51:35 +0200

    Rename about page to jobs, update navigation and job listing

--- resources/views/about.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layout>
-    <x-slot:heading>About</x-slot>
-
-    <h1>hello from the about Page</h1>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,23 @@ use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $jobs = Job::all();
-    dd($jobs);
-
-
-//    return view('home');
+    return view('home');
 });
 
-Route::get('jobs', function() {
+Route::get('jobs', function () {
     return view('jobs', [
         'jobs' => Job::all()
     ]);
 });
 
-Route::get('/about', function () {
-		return view('about');
+Route::get('jobs/{Id}', function ($Id) {
+    $job = Job::find($Id);
+
+    return view('job', [
+        'job' => $job
+    ]);
 });
 
 Route::get('/contact', function () {
-	return view('contact');
+    return view('contact');
 });
