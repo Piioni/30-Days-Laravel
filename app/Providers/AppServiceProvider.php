7c2a74ed29878c81acc5ec15d83f96aa0f6150ab
piioni <rangelc.juane@gmail.com>
2025-11-26 05:14:15 +0100

    Day 14, Pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Configurar el paginador para usar Bootstrap 5
+        // Paginator::useBootstrapFive();;
+
     }
 }
