591addb402ff5affe7188097ebcec12098df7d4b
piioni <rangelc.juane@gmail.com>
2025-11-29 04:55:29 +0100

    Day 16, CSRF Protection in first job create

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,30 @@ Route::get('/', function () {
 
 Route::get('jobs', function () {
     // 'with' se encarga del eagerLoading para evitar el problema N+1
-    $jobs = Job::with('employer')->paginate(5);
+    $jobs = Job::with('employer')->latest()->paginate(5);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('jobs/create', function () {
+    return view('jobs.create');
+});
+
 // Ruta para ver los detalles de un trabajo específico utilizando una 'wildcard
 Route::get('jobs/{Id}', function ($Id) {
-    return view('job', [
+    return view('jobs.show', [
         'job' => Job::find($Id)
+    ]);
+});
+
+Route::post('jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employerId' => '1',
+
     ]);
 });
 
